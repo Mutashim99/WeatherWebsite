ddd60c21ba779fef32169d4c9f53c70902fd0fa1
Happy <153811269+Mutashim99@users.noreply.github.com>
2024-10-12 15:12:55 +0500

    JS stuffs

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-//https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=f65269916c0cc73812afd2aee17972e9&units=metric 
+const searchButton = document.querySelector("#button-search")
+const inputfield = document.querySelector(".upper input")
 
-//API 
+searchButton.addEventListener("click" , (e)=>{
+    let inputValue = inputfield.value
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=f65269916c0cc73812afd2aee17972e9&units=metric`)
+    .then((res)=> res.json())
+    .then((data)=>{
+        console.log(data);
+    })
+})
+
